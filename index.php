35384848fbd10faeaaa88024e293b45d2b3159a6
dorrin-sot <dorrinsotoudeh123@gmail.com>
2021-12-18 00:05:29 +0330

    added everything

--- index.php
+++ index.php
@@ -1,8 +1,35 @@
 <?php
-$question = 'این یک پرسش نمونه است';
-$msg = 'این یک پاسخ نمونه است';
-$en_name = 'hafez';
-$fa_name = 'حافظ';
+$question = $msg = $en_name = $fa_name = '';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $question = $_POST["question"];
+    $en_name = $_POST["person"];
+    $fa_name = json_decode(file_get_contents('people.json'), true)[$en_name];
+    if (!str_starts_with($question, 'آیا') || !(str_ends_with($question, '?') || str_ends_with($question, '؟'))) {
+        $msg = 'سوال درستی پرسیده نشده!';
+    } else {
+        $hash = hash("sha256", $question . $fa_name);
+        $msgIndex = intval($hash[0]) % 16;
+
+        $msgsFile = fopen('messages.txt', 'r');
+        $lineNum = 0;
+        while (!feof($msgsFile)) {
+            $msg = fgets($msgsFile);
+            if ($lineNum == $msgIndex) break;
+            $lineNum = $lineNum + 1;
+        }
+        fclose($msgsFile);
+    }
+}
+
+if ($en_name == "") {
+    $persons = json_decode(file_get_contents('people.json'), true);
+    $en_name = array_keys($persons)[array_rand(array_keys($persons))];
+    $fa_name = $persons[$en_name];
+}
+if ($question == '') {
+    $msg = 'سوال خود را بپرس!';
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,10 +41,15 @@ $fa_name = 'حافظ';
 <body>
 <p id="copyright">تهیه شده برای درس کارگاه کامپیوتر،دانشکده کامییوتر، دانشگاه صنعتی شریف</p>
 <div id="wrapper">
-    <div id="title">
-        <span id="label">پرسش:</span>
-        <span id="question"><?php echo $question ?></span>
-    </div>
+    <?php
+    if ($question != "") {
+        echo
+            '<div id="title">
+                <span id="label">پرسش:</span>
+                <span id="question">' . $question . '</span>
+            </div>';
+    }
+    ?>
     <div id="container">
         <div id="message">
             <p><?php echo $msg ?></p>
@@ -30,17 +62,20 @@ $fa_name = 'حافظ';
         </div>
     </div>
     <div id="new-q">
-        <form method="post">
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
             سوال
             <input type="text" name="question" value="<?php echo $question ?>" maxlength="150" placeholder="..."/>
             را از
             <select name="person">
                 <?php
-                /*
-                 * Loop over people data and
-                 * enter data inside `option` tag.
-                 * E.g., <option value="hafez">حافظ</option>
-                 */
+                $persons = json_decode(file_get_contents('people.json'), true);
+
+                foreach ($persons as $key => $value) {
+                    echo '<option value='
+                        . $key .
+                        ($en_name == $key ? ' selected ' : '') . '>' . $value .
+                        '</option>';
+                }
                 ?>
             </select>
             <input type="submit" value="بپرس"/>
